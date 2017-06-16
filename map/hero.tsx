b9b7bd534cf113f7ab05e779d9671fe097336fc2
VillePakarinen <ville-94@hotmail.com>
2017-06-16 15:34:15 +0300

    Fixes on the map

---
+++ map/hero.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="hero" tilewidth="43" tileheight="57" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="43" height="57" source="opp-assets/sprites/humans/spr_f_archeologist_idle_anim.gif"/>
+ </tile>
+</tileset>
